4f10419823784fa1195d929c12dd03958793581f
edshen17 <shen.ed@husky.neu.edu>
2020-11-02 17:57:49 -0500

    redirect to https

--- server/index.js
+++ server/index.js
@@ -30,6 +30,16 @@ if (process.env.NODE_ENV === 'production') {
       })
 }
 
+app.use (function (req, res, next) {
+    if (req.secure) {
+            // request was via https, so do no special handling
+            next();
+    } else {
+            // request was via http, so redirect to https
+            res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => { console.log(`Server started on port ${port}`)});
