4c21458dff8221802ca4259e56a5a2f3d618c73b
edshen17 <shen.ed@husky.neu.edu>
2020-11-02 14:41:19 -0500

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-
+const serveStatic = require('serve-static');
 const app = express();
 
 // Middleware
@@ -11,18 +11,25 @@ app.use(cors());
 const words = require('./routes/api/words');
 
 app.use('/api/words', words);
-
-// Handle production
-if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public/'));
-
-    // Handle SPA
-    app.get(/.*/, (req, res) => {
-        res.sendFile(__dirname + '/public/index.html');
-    });
-}
-
+app.use(serveStatic(__dirname + "/public"));
 const port = process.env.PORT || 5000;
+const hostname = '127.0.0.1';
 
-app.listen(port, () => { console.log(`Server started on port ${port}`)});
+app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// // Handle production
+// if (process.env.NODE_ENV === 'production') {
+//     // Static folder
+//     app.use(express.static(__dirname + '/public/'));
+
+//     // Handle SPA
+
+// }
+
+
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
