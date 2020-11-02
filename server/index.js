c404617149d9bd52fd4313bf7d88aad000eaced5
edshen17 <shen.ed@husky.neu.edu>
2020-11-02 17:52:34 -0500

    remove client folder and add https redirect

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const serveStatic = require('serve-static');
+
 const app = express();
 
 // Middleware
@@ -11,25 +11,25 @@ app.use(cors());
 const words = require('./routes/api/words');
 
 app.use('/api/words', words);
-app.use(serveStatic(__dirname + "/public"));
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html');
+    });
+
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+          res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+          next()
+      })
+}
+
 const port = process.env.PORT || 5000;
-const hostname = '127.0.0.1';
 
-app.get(/.*/, (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-// // Handle production
-// if (process.env.NODE_ENV === 'production') {
-//     // Static folder
-//     app.use(express.static(__dirname + '/public/'));
-
-//     // Handle SPA
-
-// }
-
-
-
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+app.listen(port, () => { console.log(`Server started on port ${port}`)});
