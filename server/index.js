022b9e589b400ed09457c4adbb6cbce2e64a1b10
edshen17 <shen.ed@husky.neu.edu>
2020-11-02 22:36:11 -0500

    update ssl

--- server/index.js
+++ server/index.js
@@ -3,13 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 var http = require('http');
 var enforce = require('express-sslify');
-// const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(sslRedirect());
 
 const words = require('./routes/api/words');
 
@@ -22,30 +20,35 @@ if (process.env.NODE_ENV === 'production') {
 
     // Handle SPA
     app.get(/.*/, (req, res) => {
+        if(req.headers['x-forwarded-proto']!=='https'){
+        res.redirect(301, 'https://' + req.headers.host + req.url);
+        }
         res.sendFile(__dirname + '/public/index.html');
     });
 }
 
-app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
-      next()
-  })
 
-app.use (function (req, res, next) {
-    if (req.secure) {
-            // request was via https, so do no special handling
-            next();
-    } else {
-            // request was via http, so redirect to https
-            res.redirect('https://' + req.headers.host + req.url);
-    }
-});
-app.enable('trust proxy');
+
+// app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//     else
+//       next()
+//   })
+
+// app.use (function (req, res, next) {
+//     if (req.secure) {
+//             // request was via https, so do no special handling
+//             next();
+//     } else {
+//             // request was via http, so redirect to https
+//             res.redirect('https://' + req.headers.host + req.url);
+//     }
+// });
+// app.enable('trust proxy');
 
 const port = process.env.PORT || 5000;
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+// app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 http.createServer(app).listen(port, function() {
     console.log('Express server listening on port ' + app.get('port'));
