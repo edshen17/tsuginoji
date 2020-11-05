c0c7ce880520e25699764ed1b09336a53d81a039
edshen17 <shen.ed@husky.neu.edu>
2020-11-04 22:58:44 -0500

    add definition_length to fix search

--- server/routes/api/words.js
+++ server/routes/api/words.js
@@ -33,13 +33,14 @@ router.get('/:word', (req,res) => {
                 "definition": 1,
                 "pitch": 1,
                 "kana_length": { $strLenCP: "$kana" },
+                "definition_length": { $strLenCP: "$definition" },
                 "audio": 1,
                 }
             }, 
             {
                 $match: { $or: matchOrArray}
             },
-            {$sort: {"kana_length": 1, "pitch": -1, "definition": -1}},
+            {$sort: {"kana_length": 1, "definition_length": -1, "pitch": -1}},
             ]);
             const options = {
             page: req.query.page,
